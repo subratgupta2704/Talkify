7d98bcb00e847a0701068f11a1bda3da368c11a7
Subrat <subratgupta2704@gmail.com>
2025-04-02 15:36:10 +0530

    User Seed File Uploaded Successfully

--- frontend/src/components/ChatContainer.jsx
+++ frontend/src/components/ChatContainer.jsx
@@ -1,23 +1,44 @@
-import React, { useEffect, useRef } from "react";
+import { useChatStore } from "../store/useChatStore";
+import { useEffect, useRef } from "react";
+
 import ChatHeader from "./ChatHeader";
 import MessageInput from "./MessageInput";
-import { useChatStore } from "../store/useChatStore";
 import MessageSkeleton from "./skeletons/MessageSkeleton";
 import { useAuthStore } from "../store/useAuthStore";
 import { formatMessageTime } from "../lib/utils";
 
 const ChatContainer = () => {
-  const { messages, getMessages, isMessagesLoading, selectedUser } =
-    useChatStore();
-
+  const {
+    messages,
+    getMessages,
+    isMessagesLoading,
+    selectedUser,
+    subscribeToMessages,
+    unsubscribeFromMessages,
+  } = useChatStore();
   const { authUser } = useAuthStore();
   const messageEndRef = useRef(null);
 
   useEffect(() => {
     getMessages(selectedUser._id);
-  }, [selectedUser._id, getMessages]);
 
-  if (isMessagesLoading)
+    subscribeToMessages();
+
+    return () => unsubscribeFromMessages();
+  }, [
+    selectedUser._id,
+    getMessages,
+    subscribeToMessages,
+    unsubscribeFromMessages,
+  ]);
+
+  useEffect(() => {
+    if (messageEndRef.current && messages) {
+      messageEndRef.current.scrollIntoView({ behavior: "smooth" });
+    }
+  }, [messages]);
+
+  if (isMessagesLoading) {
     return (
       <div className="flex-1 flex flex-col overflow-auto">
         <ChatHeader />
@@ -25,10 +46,12 @@ const ChatContainer = () => {
         <MessageInput />
       </div>
     );
+  }
 
   return (
     <div className="flex-1 flex flex-col overflow-auto">
       <ChatHeader />
+
       <div className="flex-1 overflow-y-auto p-4 space-y-4">
         {messages.map((message) => (
           <div
@@ -68,9 +91,9 @@ const ChatContainer = () => {
           </div>
         ))}
       </div>
+
       <MessageInput />
     </div>
   );
 };
-
 export default ChatContainer;
--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -86,10 +86,18 @@ export const useAuthStore = create((set, get) => ({
   connectSocket: ()=>{
     const { authUser } = get();
     if (!authUser || get().socket?.connected) return;
-    const socket = io(BASE_URL);
+    const socket = io(BASE_URL,{
+      query: {
+        userId: authUser._id,
+      },
+    });
     socket.connect();
 
     set({socket:socket});
+
+    socket.on("getOnlineUsers", (userIds) => {
+      set({ onlineUsers: userIds });
+    });
   },
   disconnectSocket: ()=>{
     if(get().socket?.connected) get().socket.disconnect();
