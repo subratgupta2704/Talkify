2bec454688d203c1febe7e278072dd811edd2b2d
Subrat <subratgupta2704@gmail.com>
2025-04-03 15:29:31 +0530

    Production Build Project Ready For Testing

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,11 +6,10 @@ import { connectDB } from './lib/db.js';
 import cookieParser from 'cookie-parser';
 import cors from "cors";
 import { app, server } from './lib/socket.js';
+import path from "path";
 
 dotenv.config();
 
-
-// Move CORS setup here
 app.use(cors({
     origin: "http://localhost:5173",
     methods: "GET,POST,PUT,DELETE",
@@ -25,6 +24,15 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
 const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve();
+
+if(process.env.NODE_ENV==="production"){
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 server.listen(PORT, () => {
     console.log(`Server is running on ${PORT}`);
